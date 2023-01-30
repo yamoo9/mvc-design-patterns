08ee6d7fd77f1821c99812e788b8e7abc4fe1f4b
yamoo9 <yamoo9@naver.com>
2023-01-30 15:09:39 +0900

    guide. 1-4 팩토리 패턴

--- design-patterns/1-creational/1.4-factory.js
+++ design-patterns/1-creational/1.4-factory.js
@@ -5,4 +5,82 @@
 // -----------------------------------------------------------------
 
 
+// -----------------------------------------------------------------
 // 팩토리 함수 → 클래스 선택 → 객체 생성
+function createCommunityMember(type, ...args) {
+  switch(type) {
+    case PERMISSIONS.guest:
+      return new Guest(...args);
+    case PERMISSIONS.member:
+      return new Member(...args);
+    case PERMISSIONS.admin:
+      return new Administrator(...args);
+    default:
+      throw new TypeError('생성 가능한 클래스 타입은 "user", "videolecture", "comment" 입니다.');
+  }
+}
+
+class User {
+  constructor(name, age, gender) {
+    if (!name) { throw new Error('name 인수 설정은 필수입니다.'); }
+    this.name = name;
+    if (!age) { throw new Error('age 인수 설정은 필수입니다.'); }
+    this.age = age;
+    if (!gender) { throw new Error('gender 인수 설정은 필수입니다.'); }
+    this.gender = gender;
+  }
+}
+
+class Guest extends User {
+  role = PERMISSIONS.guest;
+  permission = Object.freeze({
+    read: true,
+    write: false,
+    delete: false,
+  });
+}
+
+class Member extends User {
+  role = PERMISSIONS.member;
+  permission = Object.freeze({
+    read: true,
+    write: true,
+    delete: false,
+  });
+}
+
+class Administrator extends User {
+  role = PERMISSIONS.admin;
+  permission = Object.freeze({
+    read: true,
+    write: true,
+    delete: true,
+  });
+}
+
+const PERMISSIONS = {
+  guest: 'guest',
+  member: 'member',
+  admin: 'administrator',
+};
+
+
+// -----------------------------------------------------------------
+// 팩토리 패턴을 응용한 유틸리티 함수
+
+function q(selector, context) {
+  if (!selector) {
+    throw new TypeError('selector 첫번째 매개변수는 필수 값입니다.');
+  }
+  return /^#/.test(selector)
+    ? getElementById(selector.slice(1))
+    : getElementBySelector(selector, context);
+}
+
+function getElementById(idName) {
+  return document.getElementById(idName);
+}
+
+function getElementBySelector(selector, context = document) {
+  return context.querySelector(selector);
+}

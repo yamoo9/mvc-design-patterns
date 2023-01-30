e7cf00620f6b037ddf766a8955f3e04e83341d74
yamoo9 <yamoo9@naver.com>
2023-01-30 14:42:30 +0900

    guide. 1.3 싱글톤 패턴

--- design-patterns/1-creational/1.3-singleton.js
+++ design-patterns/1-creational/1.3-singleton.js
@@ -5,10 +5,72 @@
 // -----------------------------------------------------------------
 
 
+// -----------------------------------------------------------------
 // 클래스 → 싱글톤
+class GlobalState {
+  #state = {};
+
+  constructor(initialState = {}) {
+    if (GlobalState.instance) { return GlobalState.instance; }
+    this.#state = { ...initialState };
+    Object.defineProperty(GlobalState, 'instance', { value: this });
+  }
+
+  getState(key) {
+    const state = this.#state;
+    return key ? state[key] : state;
+  }
+
+  setState(key, value) {
+    if (!key) { return; }
+    this.#state[key] = value;
+  }
+}
 
 
+// -----------------------------------------------------------------
 // IIFE → 싱글톤
+const EventBus = (() => {
+  let instance = null;
+
+  function EventBus(initialState = {}) {
+    if (instance) { return instance; }
+    this._state = { ...initialState };
+  }
+
+  EventBus.prototype = { 
+    ...EventBus.prototype, 
+    getState(key) {
+      const { _state: state } = this
+      return key ? state[key] : state;
+    },
+    setState(key, value) {
+      if (!key) { return; }
+      this._state[key] = value;
+    }
+  };
+
+  return EventBus;
+})();
 
 
+// -----------------------------------------------------------------
 // 객체 리터럴 {} === 싱글톤
+const singleton = Object.defineProperties({}, {
+  _state: {
+    value: {},
+    writable: true,
+  },
+  getState: {
+    value(key) {
+      const { _state: state } = this;
+      return key ? state[key] : state;
+    }
+  },
+  setState: {
+    value(key, value) {
+      if (!key) { return; }
+      this._state[key] = value;
+    }
+  }
+});

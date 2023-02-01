be8155add28b1733eb96598aaab81fd6b8b01e69
yamoo9 <yamoo9@naver.com>
2023-02-01 19:06:38 +0900

    guide. 커맨드 패턴

--- README.md
+++ README.md
@@ -27,7 +27,7 @@ JavaScript 디자인 패턴 학습 저장소
 시스템의 서로 다른 객체 간 통신 및 책임 할당을 개선하는데 중점을 두는 디자인 패턴을 말합니다.
 
 - [x] Chain of Responsibility
-- [ ] Command
+- [x] Command
 - [ ] Observer
 - [ ] Template Method
 - [ ] Strategy
--- design-patterns/3-behavioral/3.2-command.js
+++ design-patterns/3-behavioral/3.2-command.js
@@ -3,3 +3,34 @@
 // -----------------------------------------------------------------
 // 커맨드 패턴은 일련의 작동 및 처리를 객체로 캡슐화하는 것을 목표로 합니다.
 // -----------------------------------------------------------------
+
+
+const calculationMethods = {
+  add(x, ...numbers) {
+		return numbers.reduce((result, n) => result + n, x);
+	},
+	subtract(x, ...numbers) {
+		return numbers.reduce((result, n) => result - n, x);
+	},
+	multiply(x, ...numbers) {
+		return numbers.reduce((result, n) => result * n, x);
+	},
+	divide(x, ...numbers) {
+		return numbers.reduce((result, n) => result / n, x);
+	},
+};
+
+const calculator = {
+	execute(command, ...args) {
+		if (command in calculationMethods) {
+			return calculationMethods[command](...args);
+		}
+		return false;
+	},
+};
+
+
+console.log(calculator.execute('add', 1, 2, 7));
+console.log(calculator.execute('subtract', 5, 2, -12));
+console.log(calculator.execute('multiply', 11, 2, 9, -3));
+console.log(calculator.execute('divide', 10, 2, 8));
